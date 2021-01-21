814454a4b562936816172a7f1c446304e6fc5e9a
Maryam <maryam.kh1374@gmail.com>
2021-01-21 11:56:18 +0330

    set up migrations

---
+++ database/migrations/2021_01_21_073942_create_slots_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slots', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('start');
+            $table->dateTime('end');
+            $table->dateTime('date');
+            $table->integer('athlete_id_1')->unsigned();
+            $table->integer('athlete_id_2')->unsigned();
+            $table->integer('athlete_id_3')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slots');
+    }
+}
